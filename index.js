f9d2bdadcda5557596fd5dee899067cc8a6f5813
jeanbauerc@thoughtworks.com <jbauer@thoughtworks.com>
2018-06-05 20:34:16 -0300

    Fixes query param slice

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 var express = require('express');
 var app = express();
 
-// SD_Data_<periodo>
-app.get('/SD_Data_:periodo', function (req, res) {
+app.get('/SD_Data_:rest', function (req, res) {
+  const rest = req.params.rest;
+  const queryParams = rest.split('_');
 
-  res.send(JSON.stringify({
-    wins: 320,
-    losses: 456,
-    periodo: req.params.periodo
-  }));
+  if (rest.includes('__')) {
+    // /SD_Data_:periodo__:jogador
+  }
+
+  // 'SD_Data_:periodo_:clube_:jogador'
+  if (queryParams.length === 3) {
+    return res.send(JSON.stringify({
+      periodo: queryParams[0],
+      clube: queryParams[1],
+      jogador: queryParams[2],
+    }))
+  }
+
+  // /SD_Data_<periodo>_<clube>_<jogador>
+  // /SD_Data_<periodo>_<clube>
 });
-
-// SD_Data_<periodo>_<clube>
-// SD_Data_<periodo>__<jogador>
-// SD_Data_<periodo> _ <clube> _ <jogador>
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
